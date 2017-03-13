d5b49eaf97eb6117a6f81bce1865de89eb6b0567
Jordan Baczuk <Jordan.Baczuk@gmail.com>
2017-03-13 10:31:35 -0600

    Running watch face

---
+++ manners/build/basalt/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 0, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "manners",
+  .company = "Jordan Baczuk",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = PROCESS_INFO_WATCH_FACE | PROCESS_INFO_PLATFORM_BASALT,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x94, 0xED, 0xBE, 0xB4, 0x3D, 0x66, 0x4C, 0x12, 0x97, 0xB0, 0x00, 0x8D, 0xE2, 0x7B, 0xAD, 0x0A },
+  .virtual_size = 0xb6b6
+};
