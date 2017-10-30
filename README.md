2160c1f4b790c38497f9428c9308fbce994e9586
Jordan Baczuk <Jordan@kilfetech.com>
2017-10-29 20:07:42 -0600

    placed date and battery - need to implement battery

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # pebble-manners
 Have good manners by not turning your arm to look at the time.  A watchface with a better angle.
 
+## Set up
+- [https://developer.pebble.com/guides/tools-and-resources/pebble-tool/]()
+- [https://developer.pebble.com/docs/c/]()
+
 ## Features
 - Keep track of Mariners win/loss record and games back
 - Phone battery
--- manners/src/c/manners.c
+++ manners/src/c/manners.c
@@ -1,7 +1,12 @@
 #include <pebble.h>
+#include <time.h>
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
+static TextLayer *s_battery_layer;
+
+// TODO: add battery https://developer.pebble.com/tutorials/watchface-tutorial/part4/
 
 static void update_time() {
   // Get a tm structure
@@ -10,11 +15,18 @@ static void update_time() {
 
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-                                          "%H:%M" : "%I:%M", tick_time);
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+
+  
+  static char s_buffer2[10];
+  strftime(s_buffer2, sizeof(s_buffer2), "%m-%d", tick_time);
+  
+  static char s_buffer3[4] = "100";
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
+  text_layer_set_text(s_date_layer, s_buffer2);
+  text_layer_set_text(s_battery_layer, s_buffer3);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -25,7 +37,9 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58,52), bounds.size.w, 50));
+  s_time_layer = text_layer_create(GRect(0, 52, bounds.size.w, 50));
+  s_date_layer = text_layer_create(GRect(0, 0, bounds.size.w/2, 50));
+  s_battery_layer = text_layer_create(GRect(bounds.size.w/2, 0, bounds.size.w/2, 50));
 
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
@@ -33,12 +47,28 @@ static void main_window_load(Window *window) {
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorBlack);
+  text_layer_set_text(s_date_layer, "00-00");
+  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  
+  text_layer_set_background_color(s_battery_layer, GColorClear);
+  text_layer_set_text_color(s_battery_layer, GColorBlack);
+  text_layer_set_text(s_battery_layer, "100");
+  text_layer_set_font(s_battery_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+  text_layer_set_text_alignment(s_battery_layer, GTextAlignmentCenter);
+
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_battery_layer));
 }
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_layer);
+  text_layer_destroy(s_battery_layer);
 }
 
 static void init() {
